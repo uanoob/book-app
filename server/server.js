67e153dd991bb9aa5576901b61d26cd74bd0ea57
uanoob <usnet@ukr.net>
2018-04-05 10:10:49 +0300

    Add production env on server

--- server/server.js
+++ server/server.js
@@ -12,8 +12,17 @@ mongoose.connect(config.mongoURI);
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.use(express.static('client/build'));
+
 require('./routes/bookRoutes')(app);
 require('./routes/userRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
